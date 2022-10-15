f01293cbd2cf87576b0c029781cd32b6dcfe5507
nehabhatgithub <bhat.neha31@gmail.com>
2022-10-15 12:49:51 +0530

    Third commit

---
+++ src/main/java/com/src/pageObject/LoginPage.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+package com.src.pageObject;
+
+/**
+ * @author Admin
+ *
+ */
+
+public class LoginPage {
+
+	public LoginPage() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+}
